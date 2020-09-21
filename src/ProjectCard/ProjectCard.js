03be766a7ac3d75d450b44741676547d00217b96
Ben McClung <aurumvalian@gmail.com>
2020-09-20 18:12:00 -0600

    Add proptypes to ProjectCard

--- src/ProjectCard/ProjectCard.js
+++ src/ProjectCard/ProjectCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ProjectCard.css";
+import PropTypes from "prop-types";
 
 function ProjectCard(props) {
   return (
@@ -10,6 +11,13 @@ function ProjectCard(props) {
       <button className="delete-button" onClick={props.deleteFavorite}>Delete</button>
     </article>
   )
+}
+
+ProjectCard.propTypes = {
+  api: PropTypes.string,
+  audience: PropTypes.string,
+  link: PropTypes.string,
+  deleteFavorite: PropTypes.func
 }
 
 export default ProjectCard;
